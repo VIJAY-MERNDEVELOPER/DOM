3b399de59eb2fb4fdc0a2daead3eb355657bf788
VIJAY-MERNDEVELOPER <vijay.spm.07@gmail.com>
2023-09-25 15:47:33 +0530

    DOM

---
+++ DOM/dom.js
@@ -0,0 +1,13 @@
+console.log("working");
+
+let paraElement =document.createElement("p");
+//adding attributes
+paraElement.setAttribute("class","my-paragraph");
+
+
+console.log(paraElement);
+
+let anchorElement = document.createElement("a");
+anchorElement.setAttribute("href","path" )
+
+console.group(anchorElement)
